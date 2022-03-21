23319aabfe8162d7bdeb70b456cdf660cdbca2b2
a.a.smirnov <a.a.smirnov@tinkoff.ru>
2022-03-21 21:51:45 +0300

    docs: added javadoc comments for ConsolePrinter class

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,12 @@
+/**
+ * Служит для вывода текста в консоль
+ * @author Смирнов Антон
+ */
 public class ConsolePrinter {
+    /**
+     * Процедура, позволяющая выводить переданную строку текста в консоль
+     * @param message Строка текста, которая будет выведена в консоль
+     */
     public static void print(String message) {
         System.out.println(message);
     }
