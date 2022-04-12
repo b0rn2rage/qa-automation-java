e7663afe2028f46709032e7142ad106f747eb87e
a.a.smirnov <a.a.smirnov@tinkoff.ru>
2022-04-12 12:37:54 +0300

    fix: removed the duplicate code using the ternary operator

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -11,18 +11,16 @@ public class TimestampMessageDecorator {
     private static final int PAGE_SIZE = 2;
     /**
      * Декорирует переданное сообщение при помощи метода String.format
+     * разбивает сообщения по "страницам" при помощи разделителя "---"
+     *
      * @param message - Текст, переданный в метод
      * @return декорированное сообщение
      */
     public static String decorate(String message) {
         messageCount ++;
-        var decoratedMessage = String.format(
-                "%d %s %s", messageCount, Instant.now(), message);
-        if (messageCount % PAGE_SIZE == 0) {
-            decoratedMessage = String.format(
-                    "%d %s %s %s", messageCount, Instant.now(), message, "\n ---");
-        }
-        return decoratedMessage;
+        return messageCount % PAGE_SIZE == 0
+                ? String.format("%d %s %s %s", messageCount, Instant.now(), message, "\n ---")
+                : String.format("%d %s %s", messageCount, Instant.now(), message);
     }
 }
 
