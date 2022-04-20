46d5b01a7a6292dc79c8d64fbd09a8aca0f81aed
a.a.smirnov <a.a.smirnov@tinkoff.ru>
2022-04-20 11:15:36 +0300

    refactor: changed the signature of the "decorateAndPrintMessage" method

--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -10,15 +10,24 @@ public class MessageService {
      * Декорирует и печатает сообщения
      *
      * @param level   - Enum, отражающий важность сообщения
+     * @param message - сообщение переданное на декорирование и печать
      * @param messages - Список входящих сообщений
      */
-    public static void decorateAndPrintMessage(Severity level, String... messages) {
-        int counter = 0;
-        do {
-            String decoratedMessage = PrefixDecorator.decorate(messages[counter]) + " " +
+    public static void decorateAndPrintMessage(Severity level, String message, String... messages) {
+        String[] allMessages = new String[messages.length + 1]; // +1 добавляем так как есть обязательный message
+        allMessages[0] = message; // нулевой элемент массива = обязательный аргумент message
+
+        // Заполняем массив allMessages элементами из массива messages
+        for(int i = 1; i < messages.length + 1; i++) {
+            for (String current : messages) {
+                allMessages[i] = current;
+            }
+        }
+
+        for (String s : allMessages) {
+            String currentDecoratedMessage = PrefixDecorator.decorate(s) + " " +
                     SeverityDecorator.toString(level);
-            ConsolePrinter.print(PageSeparator.separate(decoratedMessage));
-            counter++;
-        } while (counter < messages.length);
+            ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
+        }
     }
 }
