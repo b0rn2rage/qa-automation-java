f659c10014c11d89847f451a1819660a3c779c5e
a.a.smirnov <a.a.smirnov@tinkoff.ru>
2022-04-23 16:33:57 +0300

    feat: added distinct/doubling option for MessageService also made an overload of the MessageService.print method

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,12 +1,26 @@
 package com.tcs.edu;
 
+import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.MessageService;
 import com.tcs.edu.decorator.Severity;
 
 class Application {
     public static void main(String[] args) {
-        MessageService.print(Severity.MINOR, MessageOrder.DESC, null,
-                "Hello World! 1", "Hello World! 2", "Hello World! 3");
+
+        MessageService.print((Severity) null, (String) null, null, "Hello World!", "Hello World!");
+        MessageService.print(Severity.MINOR, "random message", "Hello World! 1",
+                "Hello World! 2", "Hello World! 3");
+
+
+        MessageService.print(Severity.REGULAR, MessageOrder.ASC, "random message", "Hello World! 1",
+                "Hello World! 2", "Hello World! 3");
+        MessageService.print(Severity.MAJOR, MessageOrder.DESC, "random message", "Hello World! 1",
+                "Hello World! 2", "Hello World! 3");
+
+        MessageService.print(Severity.MAJOR, MessageOrder.ASC, Doubling.DOUBLES, "random message", "Hello World! 1",
+                "Hello World! 2", "Hello World! 3");
+        MessageService.print(Severity.MAJOR, MessageOrder.ASC, Doubling.DISTINCT, "random message", "Hello World! 1",
+                "Hello World! 2", "Hello World! 3");
     }
 }
--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -6,15 +6,13 @@ import com.tcs.edu.printer.ConsolePrinter;
  * Сервисный класс для формирования сообщений
  */
 public class MessageService {
+
     /**
-     * Декорирует и печатает сообщения
-     *
      * @param level    - Enum, отражающий важность сообщения
-     * @param order    - Каким методом будет происходить сортировка массива messages при выводе на печать
      * @param message  - Сообщение переданное на декорирование и печать
      * @param messages - Список входящих сообщений
      */
-    public static void print(Severity level, MessageOrder order, String message, String... messages) {
+    public static void print(Severity level, String message, String... messages) {
         String[] allMessages = new String[messages.length + 1];
         int counter = 0;
 
@@ -26,27 +24,114 @@ public class MessageService {
         for (String current : messages) {
             // Если элемент массива messages = null, то НЕ складываем его в массив allMessages
             if (current != null) {
-                switch (order) {
-                    case ASC:
-                        allMessages[counter++] = current;
-                        break;
-                    case DESC:
-                        // Обработка в зависимости от того, передавался ли message в метод
-                        if (allMessages[0] != null) {
-                            allMessages[messages.length + 1 - counter++] = current;
-                        } else {
-                            allMessages[messages.length - counter++] = current;
-                        }
-                        break;
-                }
+                allMessages[counter++] = current;
             }
         }
 
         for (String s : allMessages) {
             // Если в массиве остались null элементы, то НЕ выводим их на печать
             if (s != null) {
+                String currentDecoratedMessage;
+                if (level != null) {
+                    currentDecoratedMessage = PrefixDecorator.decorate(s) + " " +
+                            SeverityDecorator.toString(level);
+                } else {
+                    currentDecoratedMessage = PrefixDecorator.decorate(s);
+                }
+                ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
+            }
+        }
+
+    }
+
+
+    /**
+     * Декорирует и печатает сообщения
+     *
+     * @param level    - Enum, отражающий важность сообщения
+     * @param order    - Каким методом будет происходить сортировка массива messages при выводе на печать
+     * @param message  - Сообщение переданное на декорирование и печать
+     * @param messages - Список входящих сообщений
+     */
+    public static void print(Severity level, MessageOrder order, String message, String... messages) {
+        String[] allMessages = new String[messages.length + 1];
+
+        // В этой реализации метода print мы всегда передаем message,
+        // поэтому он всегда является первым элементом массива
+        allMessages[0] = message;
+        int counter = 0;
+
+        // Заполнить массив allMessages элементами из массива messages в зависимости от значения MessageOrder
+        while (counter < messages.length) {
+            switch (order) {
+                case ASC:
+                    for (int i = 1; i < messages.length + 1; i++) {
+                        allMessages[i] = messages[counter++];
+                    }
+                    break;
+                case DESC:
+                    for (int i = messages.length - 1; i > -1; i--) {
+                        allMessages[i] = messages[counter++];
+                    }
+                    break;
+            }
+        }
+
+        for (String s : allMessages) {
+            if (s != null) {
                 String currentDecoratedMessage = PrefixDecorator.decorate(s) + " " +
                         SeverityDecorator.toString(level);
+                ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
+            }
+        }
+    }
+
+    /**
+     * Декорирует и печатает сообщения
+     *
+     * @param level    - Enum, отражающий важность сообщения
+     * @param order    - Каким методом будет происходить сортировка массива messages при выводе на печать
+     * @param doubling - Определяет можно ли печатать на консоль одинаковые сообщения из messages
+     * @param message  - Сообщение переданное на декорирование и печать
+     * @param messages - Список входящих сообщений
+     */
+    public static void print(Severity level, MessageOrder order, Doubling doubling,
+                             String message, String... messages) {
+        String[] allMessages = new String[messages.length + 1];
+
+        // В этой реализации метода print мы всегда передаем message,
+        // поэтому он всегда является первым элементом массива
+        allMessages[0] = message;
+        int counter = 0;
+
+        // Заполнить массив allMessages элементами из массива messages
+        while (counter < messages.length) {
+            for (int i = 1; i < messages.length + 1; i++) {
+                allMessages[i] = messages[counter++];
+            }
+        }
+
+        for(int x = 0; x < allMessages.length; x ++) {
+
+            String currentDecoratedMessage = PrefixDecorator.decorate(allMessages[x]) + " " +
+                    SeverityDecorator.toString(level);
+
+            if(doubling == Doubling.DISTINCT) {
+                String[] printedMessages = new String[messages.length + 1];
+
+                // 1ое сообщение печатаем полюбому, оно не может быть дубликатом
+                ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
+                printedMessages[x] = currentDecoratedMessage;
+
+                // Итерируемся по списку напечатанных сообщений
+                // Если currentDecoratedMessage еще не печаталось, то печатаем его, иначе - игнорируем
+                for(String printMessage : printedMessages) {
+                    if(printMessage != null && !currentDecoratedMessage.equals(printMessage)) {
+                        ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
+                        printedMessages[x] = currentDecoratedMessage;
+                    }
+                }
+            } else if(doubling == Doubling.DOUBLES) {
                 ConsolePrinter.print(PageSeparator.separate(currentDecoratedMessage));
             }
         }
