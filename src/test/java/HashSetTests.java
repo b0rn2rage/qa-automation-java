a5c8220b71b0574e866a34fc9ec96178bb1fc483
a.a.smirnov <a.a.smirnov@tinkoff.ru>
2022-06-03 17:21:14 +0300

    test: add fixtures for tests

--- src/test/java/HashSetTests.java
+++ src/test/java/HashSetTests.java
@@ -3,6 +3,7 @@ import com.tcs.edu.domain.Message;
 import com.tcs.edu.repository.SavingMessageService;
 import com.tcs.edu.service.SavingMessageServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +11,23 @@ import java.util.Collection;
 import java.util.UUID;
 
 public class HashSetTests {
+    private static SavingMessageService savingMessageService;
+    private static Message message1;
+    private static Message message2;
+    private static Message message3;
+
+    @BeforeEach
+    public void setUp() {
+        message1 = new Message(Severity.REGULAR, "Hello World!1");
+        message2 = new Message(Severity.MINOR, "Hello World!2");
+        message3 = new Message(Severity.MINOR, "Hello World!3");
+        savingMessageService = new SavingMessageServiceImpl();
+    }
+
     @Test
     @DisplayName("Get message by hash key")
     public void GetMessageByHashKey() {
         //region Given
-        Message message1 = new Message(Severity.REGULAR, "Hello World!1");
-        //Message message2 = new Message(Severity.MINOR, "Hello World!2");
-        SavingMessageService savingMessageService = new SavingMessageServiceImpl();
         //endregion
         //region When
         UUID generatedKey1 = savingMessageService.log(message1);
@@ -30,10 +41,6 @@ public class HashSetTests {
     @DisplayName("Get all messages")
     public void GetAllMessages() {
         //region Given
-        Message message1 = new Message(Severity.REGULAR, "Hello World!1");
-        Message message2 = new Message(Severity.MINOR, "Hello World!2");
-        Message message3 = new Message(Severity.MINOR, "Hello World!3");
-        SavingMessageService savingMessageService = new SavingMessageServiceImpl();
         //endregion
         //region When
         savingMessageService.log(message1);
@@ -53,10 +60,6 @@ public class HashSetTests {
     @DisplayName("Get messages by severity")
     public void GetMessagesBySeverity() {
         //region Given
-        Message message1 = new Message(Severity.REGULAR, "Hello World!1");
-        Message message2 = new Message(Severity.MINOR, "Hello World!2");
-        Message message3 = new Message(Severity.MINOR, "Hello World!3");
-        SavingMessageService savingMessageService = new SavingMessageServiceImpl();
         //endregion
         //region When
         savingMessageService.log(message1);
